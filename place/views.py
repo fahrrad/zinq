99abba66b924ed8c080e2dfd966e2fe9b3119b84
fahrrad <ward.coessens@gmail.com>
2013-08-03 19:15:33 +0200

    Yea! First placed order!!!

--- place/views.py
+++ place/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from place.models import Place, Table
-from menu.models import Menu, MenuItem
+from menu.models import Menu, MenuItem, Order, OrderMenuItem
 import logging
+import string
 
 logger = logging.getLogger(__name__)
+
 
 def welcome(request):
     return render(request, "place/welcome.html")
 
+
 def menu(request, table_uuid):
-    """Typicaly called from a mobile device when scanning a qr code.
+    """Typically called from a mobile device when scanning a qr code.
      The second parameter is the unique place identifier. This corresponds to a
-     table, and uniquely identies a menu
+     table, and uniquely identifies a menu
     """
 
     if request.POST:
@@ -20,20 +23,42 @@ def menu(request, table_uuid):
         logger.info("order!")
         logger.info("Post:" + repr(request.POST))
 
+        # Loop over items in the order, and ad them to a temp collection
+        item_name_amount = []
+        for key, amount in request.POST.items():
+            if str(key).__contains__("_amount") and int(amount) > 0:
+                item_name = string.replace(key, "_amount", "")
+                logger.info("key " + item_name)
+                logger.info("amount " + amount)
+                item_name_amount.append((item_name, amount))
 
+        # place the order
+        order = place_order(item_name_amount, table_uuid)
 
         return HttpResponseRedirect("/menu/" + table_uuid)
+
     else:
         try:
             table = Table.objects.get(uuid=table_uuid)
             place = table.place
             menu = table.get_menu()
         except:
-            return render(request, "place/error.html", {'error_msg' : "No table found with id %s!" % table_uuid})
+            return render(request, "place/error.html", {'error_msg': "No table found with id %s!" % table_uuid})
 
+        return render(request, "place/menu.html", {'menu': menu,
+                                                   'place': place})
 
-        return  render(request, "place/menu.html", {'menu': menu,
-                                                'place': place})
 
 def landing(request):
     return render(request, 'landing.html')
+
+
+def place_order(items, table_uuid):
+    table = Table.objects.get(uuid=table_uuid)
+    order = Order.objects.create(table=table)
+
+    for item_name, amount in items:
+        menu_item = MenuItem.objects.get(menu=table.get_menu(), name=item_name)
+        order.addItem(menu_item, amount)
+
+    return order
