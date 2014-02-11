ba9fd5af0070efd321920c365ce4ca26be91e344
fahrrad <ward.coessens@gmail.com>
2014-02-11 08:17:17 +0100

    Setting debug mode for now

--- settings_heroku.py
+++ settings_heroku.py
@@ -13,7 +13,7 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 # QR codes for are generated
 MENU_URL = "HTTP://stormy-peak-3604.herokuapp.com/MENU/"
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -191,14 +191,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'media'
-STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
-)
