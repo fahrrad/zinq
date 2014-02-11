04f1674565702ae7d44df71dfbada04d0deff8d1
fahrrad <ward.coessens@gmail.com>
2014-02-11 08:33:49 +0100

    getting menu up and running

--- places/views.py
+++ places/views.py
@@ -193,9 +193,8 @@ def qr_codes(request, place_id):
     """Renders a view containing a QR code for every table in the places!"""
     host_prefix = "HTTP://stormy-peak-3604.herokuapp.com/MENU/"
 
-
     place = Place.objects.get(pk=int(place_id))
     table_qr_list = [host_prefix + x.pk + "/" for x in place.table_set.all()]
 
     return render(request, "places/qr_codes.html", {'table_qr_list': table_qr_list,
-                                                   'place_name': place.name})
+                                                    'place_name': place.name})
