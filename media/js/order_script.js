9ca1e12fad01d609783481b1031d54e89216d901
wardcoessens <fecon046>
2014-06-20 16:54:05 +0200

    hiding summarz and showing expanded version of an order!

---
+++ media/js/order_script.js
@@ -0,0 +1,12 @@
+$("img.3bars").click(function(){
+    var order_line_wrapper = $(this).closest(".order-line-wrapper");
+
+    // Show all summaries excelpt this one
+    $(".order-line.summary").show();
+    $(order_line_wrapper).find(".summary").hide();
+
+    // Hide all expanded except this one
+    $(".order-line.expanded").hide();
+    $(order_line_wrapper).find(".expanded").show()
+});
+
