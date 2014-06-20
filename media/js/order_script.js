fc95f9ed47fae0fa93c9f5fae9a737f3ab6621cd
wardcoessens <fecon046>
2014-06-20 19:00:15 +0200

    removed double loading of javascript code +  added timer to lines!

--- media/js/order_script.js
+++ media/js/order_script.js
@@ -1,6 +1,19 @@
 $(".order-line.expanded").hide();
 
-$("img.3bars").click(function(){
+
+function time_tick() {
+    if (window.console && window.console.log) {
+        console.log("Timetick!");
+    }
+    $(".order-line-wrapper").each(function () {
+            $(this).data("seconds", $(this).data("seconds") + 1);
+            $(this).find(".time").html($(this).data("seconds"));
+        }
+    )
+}
+
+
+$("img.3bars").click(function () {
     var order_line_wrapper = $(this).closest(".order-line-wrapper");
 
     // Show all summaries excelpt this one
@@ -12,24 +25,26 @@ $("img.3bars").click(function(){
     $(order_line_wrapper).find(".expanded").slideDown()
 });
 
-$("button.cancel_button").click(function(){
+$("button.cancel_button").click(function () {
     alert("Cancel");
 });
 
-$("button.ready_button").click(function(){
+$("button.ready_button").click(function () {
     var wrapper = $(this).closest(".order-line-wrapper");
     var order_uuid = $(wrapper).data("uuid");
     $.ajax({
         url: "/orders/d/" + order_uuid + "/",
         type: "GET"
-     }).done(function(){
-         wrapper.slideUp(400,function() {
+    }).done(function () {
+        wrapper.slideUp(400, function () {
             wrapper.remove();
-         });
+        });
 
-     }).error(function() {
-        if ( window.console && window.console.log ) {
+    }).error(function () {
+        if (window.console && window.console.log) {
             console.log("Something went south...");
         }
     });
 });
+
+$(function(){setInterval(time_tick, 1000);});
