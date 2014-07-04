508063f0beff95087a72588035de6bf31e4d6214
Frederik De Paepe <frederik@webkomeet.be>
2014-07-04 16:06:59 +0200

    ajax data fetching

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'zinq',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
@@ -157,8 +157,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'debug_toolbar',
-    'qrcode',
+    'debug_toolbar'
 )
 
 # A sample logging configuration. The only tangible logging
