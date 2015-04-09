31c25733334b3ca61bcdade526cdbce56bfa62c7
Ward Coessens <ward.coessens@gmail.com>
2015-04-09 21:11:28 +0200

    cleaning up urls, make MENU and ORDER case-insensitive

--- urls.py
+++ urls.py
@@ -14,25 +14,15 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        # Uncomment the admin/doc line below to enable admin documentation:
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
 
-                       url(r'^welcome/$', welcome),
-                       url(r'^menu/(\w{4,32})/$', menu),
-
-                       # QR codes can be encoded more efficiently when they only contain capitals.
-                       #
-                       # see http://code.google.com/p/zxing/wiki/BarcodeContents
-                       url(r'^MENU/(\w{4,32})/$', menu),
                        url(r'^$', landing),
 
-                       # view orders
-                       url(r'^orders/(\w{1,5})/$', orders),
 
-                       # Get open orders (REST call)
-                       url(r'^order/o/(\w{1,5})/$', orders_open),
-
+                       # QR codes can be encoded more efficiently when they only contain capitals.
+                       # see http://code.google.com/p/zxing/wiki/BarcodeContents.
+                       # That is why following urls are case insensitive
                        # place order
                        url(r'^order/p/(\w{4,32})/$', place_order),
 
@@ -42,11 +32,22 @@ urlpatterns = patterns('',
                        # order cancelled
                        url(r'^order/x/(\w{4,32})/$', order_cancel),
 
-                       # rest services for the order
+                       # view orders
+                       url(r'(?i)^orders/(\w{1,5})/$', orders),
+
+                       # Get open orders (REST call)
+                       url(r'^order/o/(\w{1,5})/$', orders_open),
+
+                       # View Menu
+                       url(r'^(?i)menu/(\w{4,32})/$', menu),
+
+                       # rest services for the order, to get the menu items
                        url(r'^mi/(\w{4,32})/$', menu_items),
 
-
+                       #  When ordered, check the wait status here
                        url(r'^wait/(\w{4,32})/$', wait),
                        url(r'^wait_status/(\w{4,32})/$', wait_status),
-                       url(r'^qrcodes/([0-9]{1,5})/$', qr_codes),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+                       # to get all the QR codes for a place
+                       url(r'^qrcodes/([0-9]{1,5})/$', qr_codes),) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
