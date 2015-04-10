99a25ba123f574b38a3138d7823e87095ec875ba
Ward Coessens <ward.coessens@gmail.com>
2015-04-10 21:59:15 +0200

    lingering migration out of

--- settings.py
+++ settings.py
@@ -158,7 +158,6 @@ INSTALLED_APPS = (
     # 'django_admin_bootstrapped',
     # Uncomment the next line to enable the admin, with grappelli skin
     'grappelli',
->>>>>>> sortable
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
